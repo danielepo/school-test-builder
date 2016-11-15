5648522e98468bfd3802ff3e1b41f9f15041af4f
mauriziopz <mauriziopz@gmail.com>
2016-11-15 23:42:33 +0100

    Add project files.

---
+++ BL.Test/QuestionParserTest.cs
@@ -0,0 +1,22 @@
+﻿using Entities;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Test
+{
+    [TestFixture]
+    public class QuestionParserTest
+    {
+        [Test]
+        public void EmptyStringReturnsNotNull()
+        {
+            var parse = new QuestionParser();
+            IEnumerable<Question> questions = parse.Parse("");
+            Assert.That(questions, Is.Not.Null);
+        }
+    }
+}
---
+++ BL/QuestionParser.cs
@@ -0,0 +1,17 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class QuestionParser
+    {
+        public IEnumerable<Question> Parse(string v)
+        {
+            return new List<Question>();
+        }
+    }
+}
