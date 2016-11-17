ba7713e0f9ccfb9b8bff49a255bc13e0b57deb2f
mauriziopz <mauriziopz@gmail.com>
2016-11-18 00:01:28 +0100

    Inizio generazione documento

---
+++ BL.Test/DocumentGeneratorTest.cs
@@ -0,0 +1,65 @@
+﻿using Entities;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Test
+{
+    [TestFixture]
+    public class DocumentGeneratorTest
+    {
+        DocumentGenerator generator = new DocumentGenerator();
+        //[SetUp]
+        //public void SetUp()
+        //{
+        //}
+       
+        [Test]
+        public void OnlyQuestionReturnsOne()
+        {
+            var questions = new List<Question> {
+                new Question("Domanda 1"){
+                    Answers = new List<Answer>{
+                        new Answer("risposta 1"),
+                        new Answer("risposta 2"),
+                        new Answer("risposta 3"),
+                    }
+                },
+                new Question("Domanda 2"){
+                    Answers = new List<Answer>{
+                        new Answer("risposta 1"),
+                        new Answer("risposta 2"),
+                        new Answer("risposta 3"),
+                    }
+                },
+                new Question("Domanda 3"){
+                    Answers = new List<Answer>{
+                        new Answer("risposta 1"),
+                        new Answer("risposta 2"),
+                        new Answer("risposta 3"),
+                    }
+                },
+                new Question("Domanda 4"){
+                    Answers = new List<Answer>{
+                        new Answer("risposta 1"),
+                        new Answer("risposta 2"),
+                        new Answer("risposta 3"),
+                    }
+                },
+                new Question("Domanda 5"){
+                    Answers = new List<Answer>{
+                        new Answer("risposta 1"),
+                        new Answer("risposta 2"),
+                        new Answer("risposta 3"),
+                    }
+                },
+            };
+            generator.Generate(questions);
+        }
+        
+    }
+}
---
+++ BL/DocumentGenerator.cs
@@ -0,0 +1,63 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class DocumentGenerator
+    {
+        public void Generate(IEnumerable<Question> questions)
+        {
+            using (WordprocessingDocument package = WordprocessingDocument.Create(@"C:\Users\m.pozzobon\Documents\visual studio 2015\Projects\QuizH\BL\bin\Debug\TestDocument.docx", WordprocessingDocumentType.Document))
+            {
+                MainDocumentPart mainDocumentPart = package.AddMainDocumentPart();
+
+                var body = new Body(
+                    GenerateParagraph("Welcome to my test document."),
+                    GenerateParagraph("I generated this with help from the Open XML DocumentReflector."),
+                    GenerateParagraph("What a great tool!"));
+                foreach (var question in questions)
+                {
+                    body.Append(GenerateIndented(question.Text, true));
+                    foreach (var answer in question.Answers)
+                    {
+                        body.Append(GenerateIndented(answer.Text, false));
+                    }
+                }
+                Document document = new Document(body);
+                document.Save(mainDocumentPart);
+            }
+        }
+
+        public static Paragraph GenerateIndented(string paragraphText, bool isQuestion)
+        {
+            var paragraphProperties = isQuestion ?
+                new ParagraphProperties(new NumberingProperties(new NumberingLevelReference() { Val = 0 }, new NumberingId() { Val = 1 }))
+                : new ParagraphProperties(new NumberingProperties(new NumberingLevelReference() { Val = 1 }, new NumberingId() { Val = 1 }));
+            var runProperties = isQuestion ?
+                new RunProperties(new FontSize() { Val = "22" })
+                : new RunProperties(new FontSize() { Val = "18" });
+            var element =
+                new Paragraph(paragraphProperties,
+                    new Run(runProperties,
+                        new Text(paragraphText))
+                );
+            return element;
+        }
+        public static Paragraph GenerateParagraph(string paragraphText)
+        {
+            var element =
+                new Paragraph(
+                    new Run(
+                        new Text(paragraphText))
+                );
+            return element;
+        }
+    }
+}
