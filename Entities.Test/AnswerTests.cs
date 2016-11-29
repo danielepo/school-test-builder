7352c2ffacb43e2c05586410d83019c15dc38359
danielepo <dnl.pozzobon@gmail.con>
2016-11-29 10:11:42 +0100

    Add Asnswer's Tests

---
+++ Entities.Test/AnswerTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Test
+{
+    [TestFixture]
+    public class AnswerTests
+    {
+        [Test]
+        public void AnAnswerHasATextAndPoints()
+        {
+            var answer = new Answer("irrilevant");
+            Assert.That(answer.Text, Is.EqualTo("irrilevant"));
+            Assert.That(answer.Points, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void PointsCanBeAssigned()
+        {
+            var answer = new Answer("irrilevant") { Points = 1 };
+            Assert.That(answer.Points, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void EmptyAnswerFromEmptyString()
+        {
+            var answer = Answer.FromRow("");
+            Assert.That(answer,Is.EqualTo(new Answer("")));
+        }
+        [Test]
+        public void EmptyAnswerFromPoint()
+        {
+            var answer = Answer.FromRow(".");
+            Assert.That(answer, Is.EqualTo(new Answer("")));
+        }
+        [Test]
+        public void AnswerWithText()
+        {
+            var answer = Answer.FromRow(".first:5");
+            Assert.That(answer, Is.EqualTo(new Answer("first") {Points = 5}));
+        }
+    }
+}
--- Entities/Answer.cs
+++ Entities/Answer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Odbc;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +9,16 @@ namespace Entities
 {
     public class Answer
     {
-
+        //why not use a constructor that also have the points?
         public Answer(string value)
         {
             Text = value;
         }
 
-        public string Text { get; set; }
+        public string Text { get; private set; }
         public int Points { get; set; }
-
+        // should be part of the answer or part of a validator/parser
+        // there's nothing about the answer here
         public static bool IsValid(string row)
         {
             var start = row.IndexOf('.');
@@ -27,6 +29,7 @@ namespace Entities
             return int.TryParse(prefix, out ignored);
         }
 
+        // why return always a good answer?
         public static Answer FromRow(string row)
         {
             var start = row.IndexOf('.');
@@ -41,9 +44,33 @@ namespace Entities
             }
             end -= start + 1;
             var value = row.Substring(start + 1, end).Trim();
-            return new Answer(value) {
+            return new Answer(value)
+            {
                 Points = points
             };
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Answer))
+                return false;
+
+            var that = obj as Answer;
+            return Points == that.Points &&
+                   Text == that.Text;
+        }
+
+        protected bool Equals(Answer other)
+        {
+            return string.Equals(Text, other.Text) && Points == other.Points;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((Text?.GetHashCode() ?? 0)*397) ^ Points;
+            }
         }
     }
 }
