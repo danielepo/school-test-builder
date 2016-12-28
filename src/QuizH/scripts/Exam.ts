b5ee79c3ef8eb0cc0bd05e9c9999cf902da032b6
danielepo <dnl.pozzobon@gmail.con>
2016-12-29 00:38:49 +0100

    Use type definition

--- src/QuizH/scripts/Exam.ts
+++ src/QuizH/scripts/Exam.ts
@@ -1,5 +1,9 @@
-﻿function sayHello() {
+﻿/// <reference path="../typings/modules/knockout/index.d.ts" />
+
+import ko = require("knockout");
+function sayHello() {
     const compiler = (document.getElementById("compiler") as HTMLInputElement).value;
-    const framework = (document.getElementById("framework") as HTMLInputElement).value;
+    const framework = (document.getElementById("framework") as HTMLInputElement).value
+    ko.applyBindings(new ViewModel);
     return `Hello from ${compiler} and ${framework}!`;
 }
