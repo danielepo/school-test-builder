ebf0e8f02ea0e1c0a764fc75df33d0d08f6c9afa
mauriziopz <mauriziopz@gmail.com>
2017-03-03 21:22:32 +0100

    Modifiche a Document Generator per avere un word più leggibile

--- App.Test/Class1.cs
+++ App.Test/Class1.cs
@@ -13,7 +13,7 @@ namespace App.Test
     {
 
         [Test]
-        [Ignore("Non è un vero test, serve a generare un esame")]
+        //[Ignore("Non è un vero test, serve a generare un esame")]
         public void TestMethod()
         {
             var parser = new QuestionParser();
@@ -22,7 +22,7 @@ namespace App.Test
             var questions = parser.Parse(test);
             var exam = new Exam()
             {
-                Title = "Sistema scheletrico",
+                Title = "Gravidanza e malattie esantematiche",
                 Instructions = "Leggere attentamente le domande e fare una X su una o più risposte per indicare quelle corrette. Oppure completa le parti mancanti dove opportuno."
             };
             foreach (var q in questions)
@@ -36,185 +36,240 @@ namespace App.Test
             new AnswerSheetWriter().Create(answers);
         }
 
-        string test = @"Quale dei seguenti metodi contraccettivi non è naturale:
-1.	Metodo Billings
-2.	Il diaframma:1
-3.	La temperatura basale
-La pillola del giorno dopo è un metodo contracettivo
-1.	Vero
-2.	Falso:1
-Quanti denti da latte ha un bambino
-1.	22
-2.	20:1
-3.	10
-Ci si deve lavare i denti:
-1.	Ogni volta che mangiamo
-2.	All’occorrenza
-3.	3 volte al giorno:1
-La membrana plasmatica è:
-1.	L’involucro esterno della cellula:1
-2.	Costituita da fosfolipidi:1
-3.	Immersa nel citoplasma
-Le cellule sono grandi 1 mm
-1.	Vero
-2.	Falso:1
-Dimmi la funzione dei lisosomi:
-Dimmi cosa sono i ribosomi
-I ribosomi:
-1.	Digeriscono particelle
-2.	Rappresentano la centrale energetica della cellula
-3.	Sintetizzano le proteine:1
-L’apparato del Golgi:
-1.	Sintetizza molecole di carboidrati:1
-2.	Sintetizza proteine
-3.	Digerisce particelle
-La membrana plasmatica è
-1.	Una barriera impermeabile
-2.	È selettivamente permeabile:1
-3.	Permette il trasporto passivo:1
-La fagocitosi è
-1.	Una forma di trasporto passivo
-2.	Una forma di trasporto attivo:1
-3.	Un meccanismo di osmosi
-Il nucleo plasma:
-1.	È il nucleo della cellula
-2.	È un fluido gelatinoso:1
-3.	È un fluido in cui è presente la cromatina:1
-La cromatina è costituita da solo DNA:
-1.	Vero
-2.	Falso:1
-Le cellule somatiche hanno 23 cromosomi
-1.	Vero
-2.	Falso:1
-Le cellule aploidi sono cellule sessuali
-1.	Sono cellule sessuali:1
-2.	Hanno solo 23 cromosomi
-3.	Sono cellule anomale
-Fammi un esempio di epitelio squamoso semplice:
-Il tessuto adiposo è un tessuto connettivo:
-1.	Vero:1
-2.	Falso
-Dimmi la funzione del tessuto emopoietico
-La nevroglia:
-1.	È una cellula molto grande che si trova a livello renale
-2.	Un tessuto che provvede al nutrimento dei neuroni:1
-3.	Un tessuto che provvede al sostegno dei neuroni:1
-Il tessuto muscolare cardiaco
-1.	È un tessuto connettivo
-2.	Ha una contrazione involontaria:1
-3.	Costituisce le pareti del cuore:1
-Il tessuto muscolare scheletrico
-1.	Forma i muscoli volontari:1
-2.	La sua contrazione è involontaria
-3.	Viene classificato come liscio
-Fammi un esempio di organi accessori
-Elencami i 5 strati dell’epidermide – 2 punti
-1.
-2.
-3.
-4.
-5.
-Le funzioni della cute sono:
-1.	Protezione:1
-2.	Sintesi della vitamina A
-3.	Termoregolazione
-Una persona abbronzata non ha bisogno di usare la protezione solare
-1.	Vero
-2.	Falso:1
-Le ghiandole sudoripare:
-1.	Sono ghiandole endocrine
-2.	Producono il sebo
-3.	Si trovano nella parte profonda del derma:1
-Le ghiandole sebacee sono:
-1.	Ghiandole esocrine:1
-2.	Difendono la pelle dall’umidità:1
-3.	Gli uomini ne hanno poche
-Dimmi due elementi che favoriscono l’invecchiamento cutaneo
-1.
-2.
-L’apparato locomotore è costituito da
-1.	Sistema scheletrico:1
-2.	Sistema muscolare:1
-3.	Sistema cardiaco
-4.	Sistema emuntore
-Le ossa hanno funzione:
-1.	Di deposito minerale:1
-2.	Protettiva:1
-3.	Emopoietica:1
-Alla nascita il midollo osseo appartiene totalmente alla tipologia gialla
-1.	Vero
-2.	Falso:1
-Le ossa nell’adulto sono:
-1.	178
-2.	206:1
-3.	216
-4.	Dipende dall’età
-Che cos'è l'osteoporosi:
-L'osteomalacia è
-1.	uno sviluppo anomalo dello scheletro:1
-2.	dovuto ad eccesso di vitamina D
-3.	è una patologia del bambino
-Dimmi le principali differenze tra artrosi ed artrite
+        string test = @"La donna in gravidanza non dovrebbe svolgere attività agonistica:
+1.	vero: 1
+2.	falso:0
 
+Una donna il cui stato di gravidanza è stato accertato non dovrebbe lavorare:
+1.	vero: 0
+2.	falso: 1
 
-
-
-La gotta:
-1.	è dovuta ad eccessivo consumo di carne:1
-2.	causa dolore a livello delle giunture:1
-3.	causa eccessivo accumulo di acido urico:1
-Il ginocchio può effettuare soprattutto movimenti:
-1.	di flessione:1
-2.	estensione: 1
-3.	torsione
-I menischi sono due ossa:
-1.	vero
+Una donna in sovrappeso che è in stato di gravidanza non dovrebbe seguire una dieta:
+1.	vero: 0
 2.	falso:1
-La rotula è un osso:
+
+Elencami i segni di certezza che stabiliscono lo stato di gravidanza:
+
+
+Elencami e spiegami brevemente quali sono le funzioni della placenta:
+1.	
+2.	
+3.	
+4.	
+
+L’amniocentesi è obbligatoria per le donne oltre i 40 anni?
+1.	Vero:0
+2.	falso:1
+
+Quali tra queste sono le funzioni del liquido amniotico:
+1.	favorisce il parto:1
+2.	mantiene costante la temperatura corporea:1
+3.	fornisce nutrimento al feto:0
+4.	fornisce ossigeno al feto:0
+
+Il cordone ombelicale:
+1.	collega la placenta al feto:1
+2.	è coperto da uno strato fibroso:0
+3.	permette il passaggio dell'ossigeno:1
+4.	permette l'eliminazione dell’anidride carbonica:1
+
+Cosa si intende per feto in posizione podalica:
+
+Cosa si intende per travaglio e da quali periodi è costituito:
+
+
+
+
+
+
+
+
+In quali casi è necessario il parto distocico:
+1.	feto in posizione cefalica :0
+2.	alterazione del processo fisiologico: 1
+3.	sofferenza fetale: 1
+4.	bambini di peso superiore a 3Kg: 0
+
+Che cos'è il test di Apgar e in quale momento viene eseguito:
+
+
+
+
+
+Descrivimi la malattia emolitica neonatale e la sua prevenzione:
+
+
+
+
+
+
+
+
+
+
+
+La malattia emorragica del neonato:
+1.	è più frequente nei nati prematuri:1
+2.	è dovuta a carenza di vitamina D: 0
+3.	è una patologia con decorso maligno: 0
+4.	si risolve entro alcuni giorni: 1
+
+Perché il neonato ha carenza di vitamina K?
+
+
+
+
+Elencami alcune cause che possono provocare asfissia neonatale durante la gravidanza?
+
+
+
+Durante il periodo della nascita è possibile che si verifichi asfissia neonatale a causa di:
+1.	anomalie placentari:0
+2.	aspirazione del liquido amniotico:1
+3.	rata eccessiva del parto:1
+4.	malattie cardiache della madre:0
+
+È più pericolosa l'asfissia cianotica o l'asfissia bianca?
+
+
+Mediante quale test viene rilevata una condizione di asfissia neonatale?
+
+
+
+Che cos'è la profilassi oculare?
+
+
+
+
+La vitamina K viene somministrata a tutti i neonati:
 1.	vero:1
-2.	falso
-Le ossa che compongono lo scheletro sono collegate tra loro mediante:
-1.	tendini
-2.	articolazioni
-3.	legamenti:1
-Dimmi funzione principale dei legamenti
+2.	falso:0
 
-Fammi un esempio di articolazione immobile
-Fammi un esempio di articolazione mobile
-Fammi un esempio di articolazione semimobile
-Lo scheletrico dell'arto superiore è formato da:
-1.	omero- radio-ulna+ ossa mano:1
-2.	omero-ulna-radio
-3.	omero-ulna-perone
-Lo scheletrico della gamba è formato da
-1.	omero-ulna perone
-2.	femore-tibia-perone:1
-3.	femore-metacarpo-rotula
-Dimmi qual è l'osso più lungo del corpo umano
-Lo scheletro del piede è costituito da carpo metacarpo e le falangi
-1.	vero
+Elencami tre dei principali riflessi che vengono rilevati al momento della nascita:
+1.	
+2.	
+3.	
+
+Che cosa si intende per ittero neonatale? In quale caso esso è patologico?
+
+
+
+Perché il colostro è importante?
+
+
+
+È preferibile l'allattamento al seno anche in caso di parto cesareo?
+
+Che cos'è la lussazione dell’anca? Elencami le cause che possono provocarla.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+L'indagine per la Lussazione congenita dell'anca viene eseguita:
+1.	prevalentemente sui nati in posizione podalica:0
+2.	Su tutti i neonati:1
+3.	prevalentemente sulle femmine:0
+4.	nei soggetti considerati a rischio:0
+
+Se la diagnosi di L.C.A. avviene oltre il 5° la terapia posturale con divaricatore sarà poco efficace:
+1.	vero:1
+2.	falso:0
+
+Che cosa sono le malattie esantematiche?
+1.	Patologie infettive:1
+2.	comuni negli adulti: 0
+3.	malattie per le quali esiste il vaccino: 1
+4.	malattie per le quali non è obbligatorio il vaccino:1
+
+Le malattie esantematiche più diffuse sono:
+1.	rosolia: 1
+2.	parotite:0
+3.	varicella:1
+4.	scarlatina:0
+
+Il morbillo:
+1.	colpisce prevalentemente i bambini al di sotto dei tre anni:1
+2.	la contagiosità termina dopo la scomparsa dell'esantema:0
+3.	la contagiosità inizia successivamente alla comparsa dell'esantema:0
+4.	la vaccinazione è obbligatoria:0
+
+Il vaccino del morbillo è somministrato in un’unica dose:
+1.	vero:0
 2.	falso:1
-L'osso iliaco è formato da tre ossa quali?
-Il cinto scapolare è formato da:
-La gabbia toracica permette di:
-1.	proteggere il cuore:1
-2.	proteggere i polmoni:1
-3.	respirare: 1
-Elencami le ossa della faccia
-Dimmi qual è l'unico osso mobile del cranio:
-Dimmi qual è l'unico osso del corpo che non presenta articolazioni con altre ossa:
-La scogliosi può essere curata:
-1.	mediante ginnastica agonistica
-2.	uso di corsetti:1
-3.	massaggi
-4.	interventi chirurgici:1
-L'ernia al disco deriva da:
-1.	una postura errata
-2.	scivolamento di un disco intervertebrale:1
-3.	compressione del midollo spinale:1
-4.	l'uso di droghe
-Lo scheletro del cranio comprende:
-1.	le ossa della faccia:1
-2.	le ossa del neurocranio:1";
+
+Le complicazioni dovute al morbillo sono piuttosto comuni:
+1.	vero:0
+2.	falso:1
+
+La rosolia è una patologia causata da un batterio:
+1.	vero:0
+2.	falso:1
+
+Il vaccino per la rosolia è obbligatorio:
+1.	vero:0
+2.	falso:1
+
+Tra le malattie viste quali sono teratogene?
+1.	rosolia: 1
+2.	varicella:1
+3.	epatite:0
+4.	morbillo:0
+
+La terapia specifica per la rosolia si basa sull'assunzione di antibiotici ad ampio spettro.
+1.	vero:0
+2.	falso:1
+
+La varicella contratta pochi giorni prima del parto non provoca danni per il nascituro:
+1.	vero:0
+2.	falso:1
+Dimmi quali sono le caratteristiche della varicella e qual è la sua terapia:
+
+
+
+
+
+
+Se la varicella viene contratta dopo il 4o mese di gravidanza non ci sono conseguenze per il feto:
+1.	vero:0
+2.	falso:1
+
+Che cos'è il fuoco di Sant'Antonio?
+
+
+
+La pertosse:
+1.	colpisce prevalentemente bimbi al di sotto dei 2 anni:0
+2.	è tramessa per via inalatoria: 1
+3.	può portare i lattanti a soffocamento: 1
+4.	lascia immunità permanente: 0
+
+La vaccinazione della parotite non esiste:
+1.	vero:0
+2.	falso:1
+
+La parotite è:
+1.	curata con sciroppi per la tosse:0
+2.	più pericolosa nei giovani adulti: 1
+3.	chiamata anche 'orecchioni':1
+4.	contratta per via inalatoria:1
+
+Come si può prevenire la pertosse:
+
+
+";
     }
 }
